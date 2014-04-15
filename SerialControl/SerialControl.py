7ab3510c45d942ccc354c0fe54dfcfb98ed38326
Spencer Barton <sbarton272@gmail.com>
2014-04-14 23:10:02 -0400

    Working with RFID, using Adafruit libraries

--- SerialControl/SerialControl.py
+++ SerialControl/SerialControl.py
@@ -1,20 +1,57 @@
 # Control OLED via serial
 
-import serial, time
+import serial, time, sys
+from getch import getch
 
 OLED_PORT = 3
 
+BYTE_SIZE = 1
+WORD_SIZE = 2*BYTE_SIZE # 2 bytes
+
 # Commands
-MEDIA_INIT = 'FFB1'
+MEDIA_INIT = [0xFF,0xB1]
+SS_TIMEOUT = [0x00, 0x0C, 0x00, 0x00] # stop scrolling
+CLEAR_SCREEN = [0xFF, 0xD7]
+RUN_VIDEO = [0xFF, 0xBB, 0x00, 0x00, 0x00, 0x00]  
+VIDEO_FRAME_0 = [0xFF, 0xBA, 0x00, 0x00, 0x00, 0x00, 0x00]
+VIDEO_FRAME_1 = [0xFF, 0xBA, 0x00, 0x00, 0x00, 0x00, 0x01]
 ACK = 0x06
 
 OLED = serial.Serial(OLED_PORT, 9600, timeout = 1)
 
+print getch()
+print 'done'
+
+def writeAck(cmd):
+	OLED.write(SS_TIMEOUT)
+	ack = OLED.read(BYTE_SIZE)
+	if ack != chr(ACK):
+		cmdStr = ' '.join([repr(b) for b in cmd])
+		print 'Problem with', cmdStr, 'got:', repr(ack)
+
 try:
-	OLED.write( 'FFCD0014001400143EA7' )
-	# OLED.write(MEDIA_INIT)
-	ack = OLED.read(10)
-	print repr(ack)
+	writeAck(SS_TIMEOUT)
+	
+	writeAck(CLEAR_SCREEN)
+
+	OLED.write(MEDIA_INIT)
+	ack = OLED.read(BYTE_SIZE)
+	value = OLED.read(WORD_SIZE)
+	print repr(ack), repr(value)
+	
+	# key control
+	for x in xrange(0,10): # cmd != ENTER:
+
+		print 
+
+		# if number run video that number of times
+		# if cmd is number:
+		# 	writeAck(RUN_VIDEO)
+
+		# 	writeAck(CLEAR_SCREEN)
+
+		# 	writeAck(VIDEO_FRAME_0)
+		# 	writeAck(VIDEO_FRAME_1)
 
 except Exception as e:
 	print e
